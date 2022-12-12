2f8aec50f576acdf60a8e54dc7100964bdb8b6fb
TAVI <octavianpatilea@gmail.com>
2022-12-12 13:48:32 +0200

    Update Alphabet.py

--- PCK-LAB4/Alphabet.py
+++ PCK-LAB4/Alphabet.py
@@ -4,4 +4,4 @@ import string
 class Alphabet:
     def __init__(self, blank_character: string, alphabet: string):
         self._blank_character: string = blank_character
-        self._alphabet: list = [*alphabet]
+        self._alphabet: list = [blank_character] + [*alphabet]
